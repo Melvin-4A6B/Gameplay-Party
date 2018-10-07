97a1f8af036d2abf2d7492a6a3aa400102923177
Melvin-4A6B <444277@edu.rocmn.nl>
2018-10-07 16:14:19 +0200

    Fixed login system

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -8,7 +8,7 @@
 
     public function login()
     {
-      if(isset($_REQUEST["login"]))
+      if(isset($_POST["login"]))
       {
         $this->validate(strip_tags($_POST["email"]), strip_tags($_POST["password"]));
       }

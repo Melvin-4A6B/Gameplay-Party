49fdacc170bc61b1d01ea36a9a4cfab13e03e6eb
kaan-4A6C <510507@edu.rocmn.nl>
2018-10-10 09:46:46 +0200

    reserve form kind of complete

--- app/controllers/Reserveer.php
+++ app/controllers/Reserveer.php
@@ -16,6 +16,20 @@ class Reserveer extends Controller
     public function bios($id) {
         $data["tijden"] = $this->db->getAll("SELECT * FROM reservation_times INNER JOIN cinemas on reservation_times.cinema_id = cinemas.cinema_id WHERE cinemas.cinema_id = $id");
         $data["naam"] = $this->db->getAll("SELECT cinema_name FROM cinemas  WHERE cinema_id = $id");
+        $data["disabled"] = self::disabledCheck($data['tijden']);
         Load::view("reserveForm", $data);
+    }
+
+    public function disabledCheck($data)
+    {
+        $disabled = '';
+
+        if(empty($data))
+        {
+            $disabled = "disabled";
+            $empty = "<option value=''>Geen tijden beschikbaar</option>";
+            $options = array($disabled, $empty);
+        }
+        return $options;
     }
 }

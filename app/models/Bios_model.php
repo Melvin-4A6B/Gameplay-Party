f37ccff4c0468be91d6517223dcb7fe282157db2
kaan-4A6C <510507@edu.rocmn.nl>
2018-10-19 09:08:51 +0200

    added codecomments in models map

--- app/models/Bios_model.php
+++ app/models/Bios_model.php
@@ -2,12 +2,19 @@
 
 class Bios_model
 {
-
+    /**
+     * Bios_model constructor.
+     * makes an new object called $this->db
+     */
     public function __construct()
     {
         $this->db = new Database;
     }
 
+    /**
+     * @param $query string | sql query to be executed
+     * @return $data array
+     */
     public function get($query)
     {
         $data = $this->db->getAll($query);
@@ -15,6 +22,10 @@ class Bios_model
         return $data;
     }
 
+    /**
+     * @param $table string | name of the table to insert
+     * @param $values string | values to insert in table
+     */
     public function insert($table, $values) {
         $data = $this->db->insert($table, $values);
 
--- app/models/User_model.php
+++ app/models/User_model.php
@@ -1,16 +1,25 @@
 <?php
-  class User_model {
 
+class User_model
+{
+    /**
+     * User_model constructor.
+     * makes an new object called $this->db
+     */
     public function __construct()
     {
-      $this->db = new Database;
+        $this->db = new Database;
     }
 
+    /**
+     * @param $query string | sql query to be executed
+     * @return $data array | an array with the result of the sql query
+     */
     public function check($query)
     {
-      $data = $this->db->get($query);
+        $data = $this->db->get($query);
 
-      return $data;
+        return $data;
     }
 
-  }
+}
